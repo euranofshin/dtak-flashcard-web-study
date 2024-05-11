856a5d29e607e257808ec4916a624148a0f04512
ccatherinee <ccui@college.harvard.edu>
2024-05-10 22:22:02 -0400

    oops

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, request
+import os 
+from oauth2client.service_account import ServiceAccountCredentials 
+import gspread
 
 app = Flask(__name__)
+
+credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json",['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive'])
+client = gspread.authorize(credential)
+sheet = client.open_by_key("1UfYOsQONhRGB-fyR81VWoaicpUZHkmGjfBwn4K-iBo0")
+worksheet = sheet.get_worksheet(0) 
+
 
 @app.route("/")
 def home():
@@ -8,10 +17,17 @@ def home():
 
 @app.route("/survey")
 def survey(): 
+    """
+    received_data = request.form["data"]
+    print("received pokemon", received_data)
+    worksheet.append_row(received_data)
+    """
     return render_template("survey.html")
 
-@app.route("/review")
+@app.route("/review", methods=["POST", "GET"])
 def next_page(): 
+    received_data = request.form["data"]
+    worksheet.append_row([received_data])
     return render_template("review.html")
 
 @app.route("/notification")
